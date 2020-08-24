f96057fc9f928e676de5a15e98d8373e229614f6
Aivyss <hklee6417@gmail.com>
2020-08-24 15:41:56 +0900

    Multiplication table

---
+++ JAVA08242020/src/Gugudan.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Gugudan {
+	public static void main(String[] args) { // Java main method
+		//Define variables
+		int initial = 0;
+		int finish = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		// Input values
+		System.out.print("Input initial value = ");
+		initial = sc.nextInt();
+		System.out.print("Input final value = ");
+		finish = sc.nextInt();
+		
+		// Output
+		gugudan(initial, finish);
+	}
+	
+	// Gugudan method
+	public static void gugudan(int i, int j) {
+		for (int l = i; l <= j; l++) {
+			for (int k = 1; k <= 9; k++) {
+				System.out.println(l + " x " + k + " = " + l*k);
+			}
+			System.out.println("===============");
+		}
+	}
+}
