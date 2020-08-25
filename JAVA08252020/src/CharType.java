5f386d2303cb14d5abc36fac55a07755d8db22ed
Aivyss <hklee6417@gmail.com>
2020-08-25 15:32:51 +0900

    Basic of JAVA

---
+++ JAVA08252020/src/CharType.java
@@ -0,0 +1,15 @@
+
+public class CharType {
+
+	public static void main(String[] args) {
+		char c1 = 'a';
+		char c2 = '\u0061'; // Unicode
+		char ch = '차';
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c1+0);
+		System.out.println(ch);
+	}
+
+}
---
+++ JAVA08252020/src/Detect100.java
@@ -0,0 +1,27 @@
+
+public class Detect100 {
+
+	public static void main(String[] args) {
+		// Define variables
+		int count = 0;
+		int absV = 1;
+		int sum = 0;
+		
+		// Process
+		while (sum < 100) {
+			if ((count+1)%2 == 0) {
+				sum += absV;
+				absV++;
+				count++;
+			} else {
+				sum += absV*(-1);
+				absV++;
+				count++;
+			}
+		}
+		
+		// Output
+		System.out.println("Count = " + count);
+	}
+
+}
