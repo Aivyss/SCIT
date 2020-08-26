0bed770aabf7ec4bc7b877a537ea74507ac61e7b
Aivyss <hklee6417@gmail.com>
2020-08-26 16:14:02 +0900

    Scanner and mutiplication table, sorts(bubble and selection)

---
+++ JavaBasic/src/java08262020/ScannerClass.java
@@ -0,0 +1,12 @@
+package java08262020;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+	public static void main(String args[]) {
+		// Class variables
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
