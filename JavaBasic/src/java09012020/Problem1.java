e73b89e2a5b2d2e7271690e105b390710cb7be34
Aivyss <hklee6417@gmail.com>
2020-09-01 15:52:06 +0900

    Double for example (\t)

--- JavaBasic/src/java09012020/Problem1.java
+++ JavaBasic/src/java09012020/Problem1.java
@@ -5,7 +5,7 @@ public class Problem1 {
 		// Process
 		for (int i = 1; i<=9; i++) {
 			for (int j=2; j<=9; j++) {
-				System.out.print(j + "*" + i + " = " + i*j + "    ");
+				System.out.print(j + "*" + i + " = " + i*j + " \t");
 			}
 			System.out.println();
 		}
