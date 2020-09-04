5a1e58534f8fe6b07382f6c4bb3144a67cb667b6
Aivyss <hklee6417@gmail.com>
2020-09-04 15:43:59 +0900

    for, while, if Problems

---
+++ JavaBasic/src/java09042020/Problem1.java
@@ -0,0 +1,55 @@
+/*
+ * Harshad Number Problem
+ * 12 -> 1 + 3, 12%3==0 (true)
+ * 11 -> 1 + 1, 11%2==0 (false)
+ */
+package java09042020;
+
+import java.util.Scanner;
+
+public class Problem1 {
+	public static void main(String[] args) {
+		// Define variables
+		int input;
+		int test;
+		int range = 1;
+		int sum = 0;
+		boolean tf = false;
+		Scanner sc = new Scanner(System.in);
+
+		// Input Process
+		System.out.print("하샤드 수인지 판별할 값을 입력해 주세요 : ");
+		input = sc.nextInt();
+		test = input;
+
+		// Check range
+		while (test > 0) {
+			if (range == 1) {
+				test = input - 9;
+				range = 10;
+			} else {
+				range *= 10;
+				test = input - (range - 1);
+			}
+		}
+		range = range / 10;
+
+		// Calculation 1
+		for (int i = range; i >= 1; i /= 10) {
+			test = input % i;
+			sum += test;
+			input = input - test * i;
+		}
+
+		// Calculation 2
+		if (input % sum == 0) {
+			tf = true;
+			System.out.println("하샤드 수 판별: " + tf);
+		} else {
+			tf = false;
+			System.out.println("하샤드 수 판별: " + tf);
+		}
+
+		sc.close();
+	}
+}
