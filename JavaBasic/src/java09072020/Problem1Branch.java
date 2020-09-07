f01dcb460f93ff08eb3618abb914694ad9226993
Aivyss <64432366+Aivyss@users.noreply.github.com>
2020-09-07 17:52:08 +0900

    Array Problem:Output the array of the odd number

--- JavaBasic/src/java09072020/Problem1Branch.java
+++ JavaBasic/src/java09072020/Problem1Branch.java
@@ -1,13 +1,10 @@
 package java09072020;
 
-import java.util.Scanner;
-
 public class Problem1Branch {
 	public static void main(String[] args) {
 		// Define variables
 		int[] arry = new int[50];
 		int count = 1;
-		Scanner sc = new Scanner(System.in);
 		
 		// Choice Process
 		for (int i=0; i<arry.length; i++) {
@@ -23,8 +20,6 @@ public class Problem1Branch {
 		for (int i=0; i<arry.length; i++) {
 			System.out.println(i+1 + "번째의 수 : " + arry[i]);
 		}
-		
-		sc.close();
 	}
 
 }
