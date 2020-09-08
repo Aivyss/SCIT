019e96205a2bb660a0d4ca45ef848f4332c279fc
Aivyss <64432366+Aivyss@users.noreply.github.com>
2020-09-09 01:38:32 +0900

    Hashard Number Problem (bug fix)

--- JavaBasic/src/java09042020/Problem1.java
+++ JavaBasic/src/java09042020/Problem1.java
@@ -11,7 +11,7 @@ public class Problem1 {
 	public static void main(String[] args) {
 		// Define variables
 		int input;
-		int test1, test2;
+		int test;
 		int range = 1;
 		int sum = 0;
 		boolean tf = false;
@@ -20,38 +20,36 @@ public class Problem1 {
 		// Input Process
 		System.out.print("하샤드 수인지 판별할 값을 입력해 주세요 : ");
 		input = sc.nextInt();
-		test1 = input;
+		test = input;
 
 		// Check range
-		while (test1 > 0) {
+		while (test > 0) {
 			if (range == 1) {
-				test1 = input - 9;
+				test = input - 9;
 				range = 10;
 			} else {
 				range *= 10;
-				test1 = input - (range - 1);
+				test = input - (range - 1);
 			}
 		}
 		range = range / 10;
-		test2 = input;
+		test = input;
 
-		// Calculation 1
+		// Calculation
 		for (int i = range; i >= 1; i /= 10) {
-			test1 = input / i;
-			sum += test1;
-			input = input - test1 * i;
+			int temp = test;
+			temp = temp / i;
+			sum += temp;
+			test = test - temp * i;
 		}
 
-		// Check the Hashad 2
-		if (test2 % sum == 0) {
+		// Check the Harshad Number
+		if (input % sum == 0) {
 			tf = true;
-			System.out.println("하샤드 수 판별: " + tf);
 		} else {
 			tf = false;
-			System.out.println("하샤드 수 판별: " + tf);
 		}
-		
-		System.out.println(sum);
+		System.out.println("하샤드 수 판별: " + tf);
 		sc.close();
 	}
 }
