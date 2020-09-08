57a1e850b115f2e5d5a9f8f2cafac102938ed714
Aivyss <64432366+Aivyss@users.noreply.github.com>
2020-09-08 19:08:59 +0900

    String practice

--- JavaBasic/src/java09082020/StringPractice.java
+++ JavaBasic/src/java09082020/StringPractice.java
@@ -1,25 +1,46 @@
+/*
+ * without sc.nextLine(), input process
+ * num <- sc <-integer
+ * sc(scanner object) <- enter
+ * str3 <- sc(scanner object)
+ */
 package java09082020;
 
 import java.util.Scanner;
 
 public class StringPractice {
 	public static void main(String[] args) {
-		// Define variables
+		// Define variables 1
 		String str1;
 		String str2;
 		Scanner sc = new Scanner(System.in);
 		
-		// Input test
+		// Input test 1
 		System.out.print("scanner.next() test : ");
 		str1 = sc.next(); // Do not use Space, and Input the string1
 		sc.nextLine(); // Input enter
 		System.out.print("scanner.nextLine() test : ");
 		str2 = sc.nextLine(); // input the string2
 		
-		// Output test
+		// Output test 1
 		System.out.println("scanner.next() output : " + str1);
 		System.out.println("scanner.nextLine() output : " + str2);
 		
+		// Define variables 2
+		int num;
+		String str3=null;
+		
+		System.out.println("initialization test for str3 : " + str3); // initial value = null
+		
+		// Input test 2
+		System.out.print("input integer  : ");
+		num = sc.nextInt(); // Do not use Space, and Input integer
+		System.out.print("scanner.nextLine() test : ");
+		str3 = sc.nextLine(); // input 'enter' to str3
+		
+		// Output test 2
+		System.out.println("integer : " + num);
+		System.out.println("scanner.nextLine() output : " + str3); // change null to 'enter'
 		sc.close();
 	}
 }
