3e4a5de3accbaa4c7d404e2504a323fbd925e9d6
Aivyss <hklee6417@gmail.com>
2020-09-15 07:51:49 +0900

    no message

--- JavaBasic/src/java09032020/Problem15.java
+++ JavaBasic/src/java09032020/Problem15.java
@@ -3,13 +3,13 @@ package java09032020;
 public class Problem15 {
 	public static void main(String[] args) {
 		// Define variables
-		int count = 1;
+		int count = 0;
 		int sum = 0;
 		// Process
 		while (sum<5000) {
-			sum+=count;
 			count++;
+			sum+=count;
 		}
-		System.out.println("1 ~ " + (count-1) + "까지의 합계 : " + sum);
+		System.out.println("1 ~ " + (count) + "까지의 합계 : " + sum);
 	}
 }
--- JavaBasic/src/java09042020/Problem2.java
+++ JavaBasic/src/java09042020/Problem2.java
@@ -3,21 +3,21 @@ package java09042020;
 public class Problem2 {
 	public static void main(String[] args) {
 		// Define variables
-		int books = 1000*300;
+		int books = 1000 * 300;
 		int numberOfPeople = 2;
 		int day = 0; // 일차
-		
-		while(true) {
-			books -= numberOfPeople*30;
+
+		while (true) {
+			books -= numberOfPeople * 30;
 			day++;
-			
-			if (day%10 == 0) {
-				numberOfPeople*=2;
+
+			if (day % 10 == 0) {
+				numberOfPeople *= 2;
 			}
-			if (books<=0) {
+			if (books <= 0) {
 				break;
 			}
 		}
-		System.out.println("책을 다 읽을 때까지 "+ day + "일 " + numberOfPeople + "명입니다.");
+		System.out.println("책을 다 읽을 때까지 " + day + "일 " + numberOfPeople + "명입니다.");
 	}
 }
