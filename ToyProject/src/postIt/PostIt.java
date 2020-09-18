df727b750e2100b2a078efe35fce5b3860eafe73
DESKTOP-DKO3O52\hklei <hklee6417@gmail.com>
2020-09-19 01:24:19 +0900

    ToyProject Post It application Prototype

---
+++ ToyProject/src/postIt/PostIt.java
@@ -0,0 +1,103 @@
+package postIt;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class PostIt {
+
+	// Basic of PostIt
+	private String title;
+	private String content;
+	private ArrayList<String> tags = new ArrayList<String>();
+	private int[] date = new int[6];
+	// [0]: year [1]: month [2]: date [3]: hour [4]: minute [5]: second
+
+	// To do List ON/OFF
+	private boolean toDoOnOFF;
+	private boolean toDo;
+
+	// Pin view ON/OFF
+	private boolean pin;
+
+	// Alert ON/OFF
+	boolean alertOnOff;
+	int[] alertDate = new int[6];
+
+	// Constructor
+	public PostIt() {
+		Calendar date = Calendar.getInstance();
+		this.date[0] = date.get(Calendar.YEAR);
+		this.date[1] = 1+date.get(Calendar.MONTH);
+		this.date[2] = date.get(Calendar.DAY_OF_MONTH);
+		this.date[3] = date.get(Calendar.HOUR);
+		this.date[4] = date.get(Calendar.MINUTE);
+		this.date[5] = date.get(Calendar.SECOND);
+	}
+
+	// PostItView Method
+	public void postItView() {
+		if (toDoOnOFF) {
+			if (toDo) {
+				System.out.print("/   /");
+			} else {
+				System.out.print("/ V /");
+			}
+		}
+
+		System.out.println("Title : " + title);
+		System.out.println("content : " + content);
+
+		for (int i = 0; i < tags.size(); i++) {
+			System.out.println("tag" + (i + 1) + ": " + tags.get(i));
+		}
+		
+		System.out.print("Created Date : ");
+		for (int i = 0; i < date.length; i++) {
+			System.out.print(date[i]);
+			if (i != date.length - 1) {
+				System.out.print("-");
+			}
+		}
+		System.out.println();
+
+		if (pin) {
+			System.out.print("Pin : O");
+		} else {
+			System.out.print("Pin : X");
+		}
+		System.out.println();
+
+		if (alertOnOff) {
+			for (int i = 0; i < alertDate.length; i++) {
+				System.out.print(date[i]);
+				if (i != date.length - 1) {
+					System.out.print("-");
+				}
+			}
+			System.out.println();
+		}
+	} // PostItView method end
+
+	// writePostIt Method
+	public void writePostIt() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Title : ");
+		title = sc.nextLine();
+		System.out.print("Content : ");
+		content = sc.nextLine();
+	} // writePostIt method end
+
+	// addTag Method
+	public void addTag() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Tag : ");
+		tags.add(sc.nextLine());
+	}
+
+	// tag getter
+	public ArrayList<String> getTag() {
+		return tags;
+	}
+}
---
+++ ToyProject/src/postIt/PostItStorage.java
@@ -0,0 +1,29 @@
+package postIt;
+
+import java.util.ArrayList;
+
+public class PostItStorage {
+	private ArrayList<Object> storage = new ArrayList<Object>();
+	
+	public PostItStorage() {
+		
+	}
+	
+	// storage getter
+	public ArrayList<Object> getStorage() {
+		return storage;
+	}
+	
+	// store process
+	public void store(PostIt[] input) {
+		for (int i=0; i<input.length; i++) {
+			storage.add(input[i]);
+			// storage.getStorage().get(i) --> PostIt instance
+		}
+	}
+	
+	// store process
+	public void store(PostIt input) {
+		storage.add(input);
+	}
+}
