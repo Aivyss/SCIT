40c1fb7383668e19c56cabfedc6718f3a9cb2b89
Aivyss <hklee6417@gmail.com>
2020-10-08 16:35:41 +0900

    Method Overriding and Polymorphic Variable example

--- JavaBasic/src/java10082020/MethodOverRidingExampleMain.java
+++ JavaBasic/src/java10082020/MethodOverRidingExampleMain.java
@@ -1,13 +1,25 @@
-package java10072020.overRiding;
+package java10082020;
+
+import java10072020.overRiding.Person;
+import java10072020.overRiding.Student;
 
 public class MethodOverRidingExampleMain {
 
 	public static void main(String[] args) {
 		Person person = new Person();
 		Student std = new Student("이한결", 29, "2011103149");
-
-		System.out.println(std.printAll()); // Method overRiding from Person.printAll()
-		System.out.println(std); // Method overRiding from Object.toString()
+		Person p1 = new Student("송다솔님", 26, "광주5기");
+		
+		// Method overRiding from Person.printAll()
+		System.out.println(std.printAll()); 
+		System.out.println("=============================");
+		
+		// Method overRiding from Object.toString()
 		System.out.println(person);
+		System.out.println(std); 
+		System.out.println("=============================");
+		
+		// Method Polymorphic Variable
+		System.out.println(p1); // 가상메소드
 	}
 }
