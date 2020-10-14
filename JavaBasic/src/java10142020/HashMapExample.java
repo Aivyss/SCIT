f5c79a0d03db75778e7c73fc2478dad0b7a3a4c6
Aivyss <hklee6417@gmail.com>
2020-10-14 16:05:49 +0900

    HashMap example

---
+++ JavaBasic/src/java10142020/HashMapExample.java
@@ -0,0 +1,56 @@
+/**
+ * 1. Collection
+ * 	1.1. Set
+ * 	1.2. List
+ * 2. Map
+ * 
+ * <!-- HashMap methods --!>
+ * .put(K, V) method example
+ * .keySet()
+ * .entrySet()
+ * .remove(K)
+ * .clear()
+ * .get(K)
+ */
+package java10142020;
+
+import java.util.Set;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		// Define HashMap instance
+		Map<Integer, Character> map = new HashMap<Integer, Character>();
+		
+		// .put(K, V) method
+		for (int i=33; i<100; i++) {
+			map.put(i, (char) i);
+		}
+		System.out.println(map);
+		
+		// .remove(K) method
+		map.remove(33);
+		System.out.println(map);
+		
+		
+		// .get(K) method1 - .keySet();
+		Set<Integer> keySet = map.keySet();
+		for (Integer i : keySet) {
+			System.out.println(map.get(i));
+		}
+		
+		// .get(K) method2 - .entrySet();
+		Set<Entry<Integer, Character>> ent = map.entrySet();
+		for (Entry<Integer, Character> e : ent) {
+			System.out.println("Key : " + e.getKey() + ", Value : " + e.getValue());
+		}
+		
+		// .clear() method
+		map.clear();
+		System.out.println(map);
+	}
+
+}
